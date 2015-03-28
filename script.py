8e228eb92da028b6f79f8d027d407d392a4fa4c2
Jason Gregory <JasonJG1403@gmail.com>
2015-03-28 14:41:38 -0400

    New script with less errors.

--- script.py
+++ script.py
@@ -8,25 +8,16 @@ import pickle
 
 def ldaLearn(X,y):
     # Inputs
-    # X - a N x d matrix with each row corresponding to a training example  (150 x 2)
-    # y - a N x 1 column vector indicating the labels for each training example (150 x 1)
+    # X - a N x d matrix with each row corresponding to a training example
+    # y - a N x 1 column vector indicating the labels for each training example
     #
     # Outputs
     # means - A d x k matrix containing learnt means for each of the k classes
-    # covmat - A single d x d learnt covariance matrix 
-    
+    # covmat - A single d x d learnt covariance matrix
+
     # IMPLEMENT THIS METHOD
 
-    data1 = X[:, 0]
-    data2 = X[:, 1]
-
-    # Calculate the averages
-    total1 = np.sum(data1)/data1.size
-    total2 = np.sum(data2)/data2.size
-
-    # What is k classes and if 2 how to do we build the matrix?
-
-    return means, covmat
+    return means,covmat
 
 def qdaLearn(X,y):
     # Inputs
@@ -36,51 +27,51 @@ def qdaLearn(X,y):
     # Outputs
     # means - A d x k matrix containing learnt means for each of the k classes
     # covmats - A list of k d x d learnt covariance matrices for each of the k classes
-    
+
     # IMPLEMENT THIS METHOD
 
     return means,covmats
 
-def ldaTest(means,covmat,Xtest,ytest):
+def ldaTest(means,covmat,Xtest,ytest)
     # Inputs
     # means, covmat - parameters of the LDA model
     # Xtest - a N x d matrix with each row corresponding to a test example
     # ytest - a N x 1 column vector indicating the labels for each test example
     # Outputs
     # acc - A scalar accuracy value
-    
+
     # IMPLEMENT THIS METHOD
     return acc
 
-def qdaTest(means,covmats,Xtest,ytest):
+def qdaTest(means,covmats,Xtest,ytest)
     # Inputs
     # means, covmats - parameters of the QDA model
     # Xtest - a N x d matrix with each row corresponding to a test example
     # ytest - a N x 1 column vector indicating the labels for each test example
     # Outputs
     # acc - A scalar accuracy value
-    
+
     # IMPLEMENT THIS METHOD
     return acc
 
 def learnOLERegression(X,y):
-    # Inputs:                                                         
-    # X = N x d 
-    # y = N x 1                                                               
-    # Output: 
-    # w = d x 1                                                                
-    # IMPLEMENT THIS METHOD                                                   
+    # Inputs:
+    # X = N x d
+    # y = N x 1
+    # Output:
+    # w = d x 1
+    # IMPLEMENT THIS METHOD
     return w
 
 def learnRidgeERegression(X,y,lambd):
     # Inputs:
-    # X = N x d                                                               
-    # y = N x 1 
+    # X = N x d
+    # y = N x 1
     # lambd = ridge parameter (scalar)
-    # Output:                                                                  
-    # w = d x 1                                                                
+    # Output:
+    # w = d x 1
 
-    # IMPLEMENT THIS METHOD                                                   
+    # IMPLEMENT THIS METHOD
     return w
 
 def testOLERegression(w,Xtest,ytest):
@@ -90,7 +81,7 @@ def testOLERegression(w,Xtest,ytest):
     # ytest = X x 1
     # Output:
     # rmse
-    
+
     # IMPLEMENT THIS METHOD
     return rmse
 
@@ -98,25 +89,25 @@ def regressionObjVal(w, X, y, lambd):
 
     # compute squared error (scalar) and gradient of squared error with respect
     # to w (vector) for the given data X and y and the regularization parameter
-    # lambda                                                                  
+    # lambda
 
-    # IMPLEMENT THIS METHOD                                             
+    # IMPLEMENT THIS METHOD
     return error, error_grad
 
 def mapNonLinear(x,p):
-    # Inputs:                                                                  
-    # x - a single column vector (N x 1)                                       
-    # p - integer (>= 0)                                                       
-    # Outputs:                                                                 
-    # Xd - (N x (d+1))                                                         
+    # Inputs:
+    # x - a single column vector (N x 1)
+    # p - integer (>= 0)
+    # Outputs:
+    # Xd - (N x (d+1))
     # IMPLEMENT THIS METHOD
     return Xd
 
 # Main script
 
 # Problem 1
-# load the sample data                                                                 
-X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'))            
+# load the sample data
+X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'))
 
 # LDA
 means,covmat = ldaLearn(X,y)
@@ -129,13 +120,10 @@ print('QDA Accuracy = '+str(qdaacc))
 
 # Problem 2
 
-X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'))   
+X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'))
 # add intercept
-x1 = np.ones((len(x_train),1))
-x2 = np.ones((len(x_test),1))
-
 X_i = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
-Xtest_i = np.concatenate((np.ones((X_test.shape[0],1)), X_test), axis=1)
+Xtest_i = np.concatenate((np.ones((Xtest.shape[0],1)), Xtest), axis=1)
 
 w = learnOLERegression(X,y)
 mle = testOLERegression(w,Xtest,ytest)
@@ -147,8 +135,8 @@ print('RMSE without intercept '+str(mle))
 print('RMSE with intercept '+str(mle_i))
 
 # Problem 3
-lambdas = np.linspace(0, 1.0, num=k)
 k = 21
+lambdas = np.linspace(0, 1.0, num=k)
 i = 0
 rmses3 = np.zeros((k,1))
 for lambd in lambdas:
@@ -162,7 +150,7 @@ lambdas = np.linspace(0, 1.0, num=k)
 k = 21
 i = 0
 rmses4 = np.zeros((k,1))
-opts = {'maxiter' : 50}    # Preferred value.                                                
+opts = {'maxiter' : 50}    # Preferred value.
 w_init = np.zeros((X_i.shape[1],1))
 for lambd in lambdas:
     args = (X_i, y, lambd)
